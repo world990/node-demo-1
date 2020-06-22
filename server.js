0ab607c5d0e2a961fcc68ea3d542e05f6f7f44ce
world990 <342721572@qq.com>
2020-06-22 22:37:49 +0800

    更新

--- server.js
+++ server.js
@@ -26,7 +26,14 @@ var server = http.createServer(function (request, response) {
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`二哈\n`);
+    response.write(`
+    <!DOCTYPE html>
+    <head>
+    <link rel="stylesheet" href="/x"
+    </head>
+    <body>
+    <h1>哈哈哈</h1>
+    </body>`);
     response.end();
   } else if (path === "/x") {
     response.statusCode = 200;
@@ -36,7 +43,7 @@ var server = http.createServer(function (request, response) {
   } else {
     response.statusCode = 404;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`你输入的路径不存在对应的内容`);
+    response.write(`你访问的页面不存在`);
     response.end();
   }
 
